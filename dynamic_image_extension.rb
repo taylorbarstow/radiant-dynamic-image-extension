6155a79e33d9d1b70d7ca07a8984b7da18c90c5e
Roman Simecek <roman.simecek@screenconcept.ch>
2009-02-23 15:59:49 +0100

    modified the version number and added a url to the repository

--- dynamic_image_extension.rb
+++ dynamic_image_extension.rb
@@ -1,7 +1,7 @@
 class DynamicImageExtension < Radiant::Extension
-  version "0.1"
+  version "0.2"
   description "This is an extension to dynamically generate images from text for use on your page."
-  url ""
+  url "http://github.com/simerom/radiant-dynamic-image-extension/tree/master"
   
   def activate
     Page.class_eval do
